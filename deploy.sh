740490ebf6587a47ba1bb4cb10e4e9c1a60ca01f
loomhigh223555 <45379144+loomhigh223555@users.noreply.github.com>
2024-12-18 16:55:47 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Temporary hack to enable automatic deployments
-rsync -a --delete /home/glass/plugorgau.github.io/_site/ /var/www/plug.org.au/
-ln -s /srv/efs/sites/plug.org.au/media/ /var/www/plug.org.au/media
-ln -s /srv/efs/sites/plug.org.au/media/video /var/www/plug.org.au/video
-chown -R www-data:www-data /var/www/plug.org.au/
+#rsync -a --delete /home/glass/plugorgau.github.io/_site/ /var/www/plug.org.au/
+#ln -s /srv/efs/sites/plug.org.au/media/ /var/www/plug.org.au/media
+#ln -s /srv/efs/sites/plug.org.au/media/video /var/www/plug.org.au/video
+#chown -R www-data:www-data /var/www/plug.org.au/
+
+# I DO NOT KNOW WHAT THE PURPOSE OF THIS IS
